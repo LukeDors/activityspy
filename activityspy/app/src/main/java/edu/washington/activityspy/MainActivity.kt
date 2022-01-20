e093303e41e092a7d3886857bbada4a08374a608
LukeDors <ldorsett@uw.edu>
2022-01-19 21:41:07 -0600

    Added log messages, logs, screenshot

--- activityspy/app/src/main/java/edu/washington/activityspy/MainActivity.kt
+++ activityspy/app/src/main/java/edu/washington/activityspy/MainActivity.kt
@@ -7,6 +7,39 @@ import android.util.Log
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i("MainActivity", "onCreate event fired")
+        Log.i("MainActivity", "savedInstanceState is " + savedInstanceState.toString())
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity", "onStart event fired")
+    }
+
+    override fun onPause() {
+        super.onStart()
+        Log.i("MainActivity", "onPause event fired")
+    }
+
+    override fun onResume() {
+        super.onStart()
+        Log.i("MainActivity", "onResume event fired")
+    }
+
+    override fun onStop() {
+        super.onStart()
+        Log.i("MainActivity", "onStop event fired")
+    }
+
+    override fun onRestart() {
+        super.onStart()
+        Log.i("MainActivity", "onRestart event fired")
+    }
+
+    override fun onDestroy() {
+        super.onStart()
+        Log.i("MainActivity", "onDestroy event fired")
+        Log.e("MainActivity", "We're going down, Captain!")
     }
 }
